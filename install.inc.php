80cfc5f1fc9a4d6002822ada6fc8a277c4466659
Markus Staab <maggus.staab@googlemail.com>
2017-01-20 14:48:44 +0100

    added rex/php min version check

--- install.inc.php
+++ install.inc.php
@@ -10,4 +10,17 @@
  * file that was distributed with this source code.
  */
 
-$REX['ADDON']['install']['yconverter'] = true;
+$minPhpVersion = '5.4.0';
+$minRexVersion = '4.6.0';
+if (version_compare(PHP_VERSION, $minPhpVersion) < 0) {
+    $msg = 'Es wird mindestens PHP '. $minPhpVersion .' benötigt!';
+} elseif (version_compare($REX['VERSION'] . '.' . $REX['SUBVERSION'] . '.' . $REX['MINORVERSION'], $minRexVersion) < 0) {
+    $msg = 'Es wird mindestens REDAXO '. $minRexVersion .' benötigt!';
+}
+
+if ($msg != '') {
+    $REX['ADDON']['installmsg']['yconverter'] = $msg;
+    $REX['ADDON']['install']['yconverter'] = false;
+} else {
+    $REX['ADDON']['install']['yconverter'] = true;
+}
