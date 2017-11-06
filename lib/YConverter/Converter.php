b1059a4cdac44543f107d43b90e00e9efe69813c
Tobias Krais <tk@design-to-use.de>
2017-11-06 10:07:37 +0100

    Community rename failsafe

--- lib/YConverter/Converter.php
+++ lib/YConverter/Converter.php
@@ -207,7 +207,9 @@ class Converter
                 // Community Addon -> YCom
                 'replaces' => [
                     ['checkperm\(' => 'checkPerm('],
-                    ['community' => 'ycom'],
+                    ['rex_addon::get\((["']{1})community$1\)->isActivated\(\)' => 'rex_addon::get(\'ycom\')->isActivated()'],
+                    ['rex_addon::get\((["']{1})community$1\)->isAvailable\(\)' => 'rex_addon::get(\'ycom\')->isAvailable()'],
+                    ['rex_addon::get\((["']{1})community$1\)->isInstalled\(\)' => 'rex_addon::get(\'ycom\')->isInstalled()'],
                     ['rex_com_auth\s*::' => 'rex_ycom_auth::'],
                 ]
             ],
