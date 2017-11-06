c574eb3c3f69361d04dd88ea3db0397fe6552049
Tobias Krais <tk@design-to-use.de>
2017-11-06 11:40:40 +0100

    Community Addon -> YCom (#25)
    Ersetzungen vom Community zum YCom AddOn hinzugefügt

--- lib/YConverter/Converter.php
+++ lib/YConverter/Converter.php
@@ -203,6 +203,15 @@ class Converter
                     ['seo42\s*::\s*getLangCode\((.*?)\)' => 'rex_clang::getCurrent()->getCode()'],
                     ['seo42\s*::\s*getBaseUrl\((.*?)\)' => 'rex::getServer()'],
                 ]
+            ], [
+                // Community Addon -> YCom
+                'replaces' => [
+                    ['checkperm\(' => 'checkPerm('],
+                    ['rex_addon::get\((["']{1})community$1\)->isActivated\(\)' => 'rex_addon::get(\'ycom\')->isActivated()'],
+                    ['rex_addon::get\((["']{1})community$1\)->isAvailable\(\)' => 'rex_addon::get(\'ycom\')->isAvailable()'],
+                    ['rex_addon::get\((["']{1})community$1\)->isInstalled\(\)' => 'rex_addon::get(\'ycom\')->isInstalled()'],
+                    ['rex_com_auth\s*::' => 'rex_ycom_auth::'],
+                ]
             ],
         ];
 
